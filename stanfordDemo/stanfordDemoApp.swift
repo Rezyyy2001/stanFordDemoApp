6dd1a0af46717b29a5a8e515ae26b8f1dabe40bb
Rezyyy2001 <92544727+Rezyyy2001@users.noreply.github.com>
2024-03-14 19:50:45 -0400

    Initial Commit

---
+++ stanfordDemo/stanfordDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  stanfordDemoApp.swift
+//  stanfordDemo
+//
+//  Created by Rezka Yuspi on 3/14/24.
+//
+
+import SwiftUI
+
+@main
+struct stanfordDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
