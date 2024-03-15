4b4ccbe6e27c882b47bbdbf3f09412c3771eeff6
Rezyyy2001 <92544727+Rezyyy2001@users.noreply.github.com>
2024-03-14 20:29:43 -0400

    made four cardviews

--- stanfordDemo/ContentView.swift
+++ stanfordDemo/ContentView.swift
@@ -2,20 +2,42 @@
 //  ContentView.swift
 //  stanfordDemo
 //
-//  Created by Rezka Yuspi on 3/14/24.
+//  Created by Rezka Yuspi on 3/13/24.
 //
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        HStack {
+            Cardview(isFaceUp: true)
+            Cardview(isFaceUp: true)
+            Cardview(isFaceUp: true)
+            Cardview(isFaceUp: false)
         }
         .padding()
+        .foregroundColor(.pink)
+    }
+}
+
+struct Cardview: View{
+    var isFaceUp: Bool = false
+    
+    var body: some View {
+        ZStack {
+            let base = RoundedRectangle(cornerRadius: 12)
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 12)
+                    .foregroundColor(.white)
+                RoundedRectangle(cornerRadius: 12)
+                    .strokeBorder(lineWidth: 4)
+                    .foregroundColor(.pink)
+                Text("👀").font(.largeTitle)
+            } else {
+                RoundedRectangle(cornerRadius: 12)
+                    
+            }
+        }
     }
 }
 
